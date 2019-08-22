a059a554f1e64a0ef40a89c0293c21601ef1a1a2
Jakub Piecuch <j.piecuch96@gmail.com>
2019-08-22 13:59:15 +0200

    build-functions tweaks

--- docker/build-env/build-functions.sh
+++ docker/build-env/build-functions.sh
@@ -8,6 +8,12 @@ setup () {
 
 build () {
   pushd /vagrant
-  make -j${JOBS:-4} BUILD_MODE=${1:-default}
+  if [ -n "$1" ]; then
+    make -j${JOBS:-4} BUILD_MODE=$1
+  else
+    make -j${JOBS:-4} byte
+  fi
   popd
 }
+
+export PATH=/vagrant/infer/bin:$PATH
